b65d73df54fe1cb49dd0a3b57ff38b9cef07e541
AgustinFrich <agustinfrich@gmail.com>
2024-05-27 22:02:22 -0300

    vercel config

--- app/index.php
+++ app/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once '../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
